deb1528ca91d2974a2caf961c464d55e3c3b1508
Apple <apple@Apples-MacBook-Pro.local>
2023-11-02 01:38:03 +0530

    changes made on p41

--- p41-infra/eks/variables.tf
+++ p41-infra/eks/variables.tf
@@ -6,7 +6,7 @@ variable "vpc_cidr" {
 variable "azs" {
   type        = list(string)
   description = "A list of availability zones in the region."
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"] # Replace with your region's availability zones
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "cluster_name" {
@@ -37,13 +37,13 @@ variable "single_nat_gateway" {
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"] # Customize as needed
+  default     = ["10.0.1.0/24", "10.0.2.0/24"] 
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"] # Customize as needed
+  default     = ["10.0.3.0/24", "10.0.4.0/24"] 
 }
 
-# Add other variables as needed for your VPC configuration
+
